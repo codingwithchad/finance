9b62dcdab5158cf9bed334ba793a22c4f0350653
Chad H <chadg1980@gmail.com>
2021-06-13 16:13:22 -0700

    added header and section for Category

--- finance/finance.py
+++ finance/finance.py
@@ -1,9 +1,21 @@
 import csv
 
-with open('..\\datafiles\\April2021.csv') as csvFile:
+with open('..\\datafiles\\April2021.csv') as csvFile,\
+        open('..\\datafiles\\edited\\April2021edited.csv', 'w') as fout:
     datareader = csv.reader(csvFile, delimiter=',')
+    datawriter = csv.writer(fout)
+    try:
+        header = next(datareader)
+        header.insert(3, 'category')
+        datawriter.writerow(header)
+    except StopIteration as e:
+        pass
+
     for row in datareader:
-        print(', '.join(row))
+        if len(row) < 1:
+            continue
+        row.insert(3, "Category Goes Here")
+        datawriter.writerow(row[0:5])
 
 
 
